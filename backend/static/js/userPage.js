d1430c3dd1bc23633b84bea7198e5953c1d36cbc
francescobassi2002 <francesco.bassignana@itiscuneo.eu>
2021-04-17 20:25:04 +0200

    .

---
+++ backend/static/js/userPage.js
@@ -0,0 +1,84 @@
+url = new URL(window.location.href);
+const vett = url.pathname.split('/');
+const user = vett[2];
+const card = $('#rspi').text();
+
+function togglePopup() {
+  var popup = document.getElementById('popup');
+  popup.classList.toggle('active');
+}
+//_______________________________________________________________________________
+
+$(document).ready(async () => {
+  console.log('ready');
+  console.log(url);
+  console.log(vett);
+
+  const res = await fetch(`${url.origin}/RSPi?user=${user}`);
+  const resj = await res.json();
+  console.log(resj);
+  if (resj.status != '200') {
+    alert(`errore: ${resj.message}`);
+  } else {
+    const data = resj.data;
+    console.log(data);
+
+    for (rspi of data) {
+      $('#contenitore').append(
+        card.replace('%mac%', rspi.name ? rspi.name : rspi.mac)
+      );
+    }
+  }
+});
+
+//_______________________________________________________________________________
+$('#logout').click(async () => {
+  console.log('ciao');
+  const rowRes = await fetch(`/user/logOut`);
+  const res = await rowRes.json();
+
+  if (res.status == '200') {
+    console.log(res.status);
+    window.location.href = 'login';
+  }
+});
+//_______________________________________________________________________________
+$('#add').click(async () => {
+  window.location.href = `${url.origin}/view/${user}/rspreg`;
+});
+
+$('#rspiList').click(async () => {
+  const res = await fetch(`${url.origin}/RSPi?user=${user}`);
+  const resj = await res.json();
+  console.log(resj);
+  if (resj.status != '200') {
+    alert(`errore: ${resj.message}`);
+  } else {
+    const data = resj.data;
+    console.log(data);
+
+    for (rspi of data) {
+      $('#contenitore').append(
+        card.replace('%mac%', rspi.name ? rspi.name : rspi.mac)
+      );
+    }
+  }
+});
+
+[$('#rspiList'), $('#userInfo'), $('#add'), $('#logout')].forEach((el) => {
+  el.click(() => {
+    $('#contenitore').empty();
+
+    el.attr('class', 'active');
+    el.siblings().attr('class', '');
+  });
+});
+
+$('#userInfo').click(async () => {
+  Res = await fetch(`${url.origin}/user`);
+  res = await Res.json();
+  if (res.status != '200') {
+    alert(`Error : ${res.status}: ${res.message}`);
+  } else {
+  }
+});
